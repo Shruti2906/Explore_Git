f5a80ffa3d3418ce3adcdb3536d0d29fcfdb42a5
Shruti <65824891+Shruti2906@users.noreply.github.com>
2025-07-27 23:37:16 +0530

    Feature branch (#3)
    * Commit 1
    
    * Commit 2

--- main.js
+++ main.js
@@ -10,4 +10,6 @@ console.log("a: ", a);
 console.log("b: ", b);
 console.log("c: ", c);
 console.log("d: ", d);
+console.log("This commit is to check squash & merge");
+console.log("This commit is to check squash & merge 2");
 
